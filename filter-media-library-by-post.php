944b0073727f034aac5e239260c1938fd4be98a3
RayQ <ray@qstudio.us>
2020-11-03 20:50:57 +0000

    0.0.2

--- filter-media-library-by-post.php
+++ filter-media-library-by-post.php
@@ -4,7 +4,7 @@
  * Plugin Name:     Filter Media Library by Post
  * Plugin URI:      http://qstudio.us/
  * Description:     Filter the lits of items in the WordPress Media Library by the post they are attached to
- * Version:         0.0.1
+ * Version:         0.0.2
  * Author:          Q Studio
  * Author URI:      http://qstudio.us
  * License:         GPL2
@@ -17,17 +17,16 @@ defined( 'ABSPATH' ) OR exit;
 if ( ! class_exists( 'Filter_Media_Library_By_Post' ) ) {
     
     // instatiate plugin via WP plugins_loaded - init was too late for CPT ##
-    add_action( 'plugins_loaded', array ( 'Filter_Media_Library_By_Post', 'get_instance' ), 0 );
+    \add_action( 'plugins_loaded', array ( 'Filter_Media_Library_By_Post', 'get_instance' ), 0 );
     
     class Filter_Media_Library_By_Post {
         
         // version ##
-        const version = '0.0.1';
+        const version = '0.0.2';
         
-        // for translation ##
-        static $text_domain = 'filter-media-library-by-post';
-        static $post_types = array( 'property' );
-        
+        // post types ##
+		static $post_types = [ 'page', 'post' ];
+
         // Refers to a single instance of this class. ##
         private static $instance = null;
         
@@ -53,43 +52,176 @@ if ( ! class_exists( 'Filter_Media_Library_By_Post' ) ) {
          * @since       0.2
          * @return      void
          */
-        private function __construct() 
-        {
-         
+        private function __construct(){
+		 
             // filter the query ##
-            add_filter( 'parse_query', array( __CLASS__, 'parse_query' ) );
+            \add_filter( 'parse_query', array( __CLASS__, 'parse_query' ), 1 );
 
             // build the <select> dropdown ##
-            add_action( 'restrict_manage_posts', array( __CLASS__, 'restrict_manage_posts' ) );
-            
-        }
-        
+			\add_action( 'restrict_manage_posts', array( __CLASS__, 'restrict_manage_posts' ), 1 );
+			
+		}
+		
+
+		public static function log( $log ){
+
+			// local testing only ##
+			if( ! class_exists( '\q\core\helper' ) ){ return false; }
+
+			\q\core\helper::log( $log );
+
+		}
+		
+		
+
+		public static function get_post_types(){
+
+			// filter posts types to include ##
+			$post_types = \apply_filters( 'q/filter_media_library_by_post/post_type', self::$post_types );
+
+			// escape for SQL - done here due to bug of adding string of integer values ##
+			$post_types = array_map(function($v) {
+				return "'" . esc_sql($v) . "'";
+			}, $post_types);
+
+			// we need to return a csv string ##
+			$return  = implode( ',', $post_types );
+
+			// self::log( $return );
+
+			// kick it back ##
+			return $return;
+
+		}
+
+
+		public static function get_attachments(){
+
+			global $wpdb;
+			
+			$array = $wpdb->get_results( 
+				"
+					select *
+					FROM {$wpdb->posts}
+					WHERE post_type = 'attachment'
+					ORDER BY post_date
+				"
+			);
+
+			// self::log( $array );
+			
+			return $array;
+
+		}
+
+
+		public static function get_parents(){
+
+			// get attachments ##
+			$attachment_query = self::get_attachments();
+
+			// save parents ##
+			$parents = [];
+
+			// loop over to get unique parents ##
+			// @todo - this could be moved to a single SQL query ##
+			foreach( $attachment_query as $post ) {
+
+				// add parent to parents ##
+				$parents[] = $post->post_parent;
+
+			}
+
+			// drop duplicates ##
+			$parents = array_unique( $parents );
+
+			// escape for SQL - done here due to bug of adding string of integer values ##
+			$parents = array_map(function($v) {
+				return esc_sql($v);
+			}, $parents);
+
+			// we need to return a csv string ##
+			$return  = implode( ',', $parents );
+
+			// self::log( $return );
+
+			// kick it back ##
+			return $return;
+
+		}
+
+
+
+		public static function get_posts_with_attachments(){
+
+			global $wpdb;
+			
+			$array = $wpdb->get_results( 
+				"
+					select *
+					FROM {$wpdb->posts}
+					WHERE ID IN (".self::get_parents().")
+					&& post_type IN (".self::get_post_types().")
+					ORDER BY post_date
+				"
+			);
+
+			// self::log( $wpdb->last_query );
+			
+			return $array;
+
+		}
+
+
+
+		public static function get_attachment_count( $post_parent = null ){
+
+			global $wpdb;
+			
+			$count = $wpdb->get_var( $wpdb->prepare(
+				"
+					select COUNT(*) 
+					FROM {$wpdb->posts}
+					WHERE post_parent = '%d'
+					&& post_type = 'attachment'
+				",
+				$post_parent
+				)
+			);
+
+			// self::log( $wpdb->last_query );
+			
+			return $count;
+
+		}
+
+
         
         /**
          * Set query variables in $wp_query
          * 
          * @since       0.1
          */
-        public static function parse_query( $wp_query ) 
-        {
+        public static function parse_query( $wp_query ){
 
+			// get the global $pagenow value ##
             global $pagenow;  
 
-            if ( 'upload.php' === $pagenow ) {
+			// check we're on the upload view ##
+            if (
+				\is_admin() 
+				&& 'upload.php' === $pagenow
+				&& isset( $_GET['q_filter_parent'] ) 
+				&& $_GET['q_filter_parent'] != '' 
+			) {
 
-                if ( is_admin() && isset( $_GET['post_id'] ) && $_GET['post_id'] != '' ) {
+					// sanitize ##
+					$q_filter_parent = \sanitize_key( $_GET['q_filter_parent'] );
 
-                    $original_query = $wp_query;
-
-                    $wp_query->set('post_parent', $_GET['post_id']);
-
-                    $wp_query = $original_query;
-
-                    wp_reset_postdata();
+					// set a post parent ##
+                    $wp_query->set( 'post_parent', $q_filter_parent );
 
                 }
-
-            }
 
         }
 
@@ -100,61 +232,39 @@ if ( ! class_exists( 'Filter_Media_Library_By_Post' ) ) {
          * @since       0.1
          * @link        http://wpquestions.com/question/show/id/8268
          */
-        public static function restrict_manage_posts()
-        {
+        public static function restrict_manage_posts(){
 
+			// bring in some globals ##
             global $pagenow, $wp_query;
 
+			// upload check ##
             if ( 'upload.php' == $pagenow ) {
 
-                // get current ID ##
-                $current= isset( $_GET['post_id'] ) ? $_GET['post_id'] : '';
-                
-                #$queried_object = get_queried_object();
-                #pr( get_query_var() );
+                // get current attachment parent ID -- if set ##
+                $current = isset( $_GET['q_filter_parent'] ) ? \sanitize_key( $_GET['q_filter_parent'] ) : 0 ;
 
-                // prepare $args ##
-                $args = array (
-                    'post_type'         => self::$post_types,
-                    'posts_per_page'    => -1,
-                    'order'             => 'DESC',
-                    'orderby'           => 'title',
-                    'post_status'       => 'publish'
-                );
-
-                // don't mess with this query - please ## 
-                #wp_reset_query();
-                #wp_reset_postdata();
-
-                $media_query = new WP_Query( $args );
-                #pr( $wp_query );
-                #pr( $media_query->query );
-                #pr( $media_query->found_posts );
+				// get posts with attachments ##
+				$posts_with_attachments = self::get_posts_with_attachments();
 
 ?>
-                <select name="post_id">
-                    <option value=""><?php _e ( 'Uploaded To', self::$text_domain ); ?></option>
+                <select name="q_filter_parent">
+                    <option value=""><?php _e ( 'Uploaded To', 'filter-media-library-by-post' ); ?></option>
 <?php
 
-                    while ( $media_query->have_posts() ) {
-
-                        $media_query->the_post();
+					foreach( $posts_with_attachments as $post_parent ) {
 
                         // highligh current selection ##
-                        $selected = ( $current == $media_query->post->ID ) ? $selected = ' selected="selected"' : '' ;
-
-                        // count ##
-                        $count = count( get_children( array( 'post_parent' => $media_query->post->ID ) ) );
-
-                        if ( $count > 0 ) {
+						$selected = ( $current == $post_parent->ID ) ? $selected = ' selected="selected"' : '' ;
+						
+						// get attachment count ##
+						$count = self::get_attachment_count( $post_parent->ID ) ;
+						// self::log( 'attachment count for post: '.$post_parent->ID.' --> '.$count );
 
 ?>
-                    <option value="<?php echo $media_query->post->ID; ?>"<?php echo $selected; ?>>
-                        <?php echo $media_query->post->post_title; ?> ( <?php echo $count; ?> )
+                    <option value="<?php echo $post_parent->ID; ?>"<?php echo $selected; ?>>
+                        [ <?php echo $post_parent->post_type; ?> ] <?php echo $post_parent->post_title; ?> ( <?php echo $count; ?> )
                     </option>
 <?php
-
-                        }
 
                     }
 
@@ -162,7 +272,6 @@ if ( ! class_exists( 'Filter_Media_Library_By_Post' ) ) {
                 </select>
 <?php
 
-                wp_reset_postdata();
 
             }
 
